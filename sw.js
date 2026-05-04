c5910ab8cd46dfb81ee4d69507ee3a14449a7cca
Schmoogan <sithdan204@gmail.com>
2026-05-03 21:35:19 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'nonneg-v1';
+const ASSETS = [
+  '/daily-tracker/',
+  '/daily-tracker/index.html',
+  '/daily-tracker/manifest.json',
+  '/daily-tracker/icon-192.png',
+  '/daily-tracker/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
